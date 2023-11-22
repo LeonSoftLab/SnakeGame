118be163e02a8d61d790417e6ccf30361c0dbe32
Leon <87311850+LeonSoftLab@users.noreply.github.com>
2023-11-22 22:28:41 +0100

    test version 0.1
    bearbeiten classes

--- settings.py
+++ settings.py
@@ -6,4 +6,11 @@ ORANGE = (255, 165, 0)
 GREEN = (40, 225, 70)
 RED = (255, 0, 0)
 FPS = 60
+ICON_PATH = "res/snake.png"
 VECTORS = ("left", "up", "right", "down")
+direction_mapping = {
+    "left": (-1, 0),
+    "right": (1, 0),
+    "up": (0, -1),
+    "down": (0, 1)
+}
