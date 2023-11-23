a76b85e8c70abbeeb11a54ffc09570d44011222f
Leon <87311850+LeonSoftLab@users.noreply.github.com>
2023-11-23 11:19:48 +0100

    ver 1.0
    Bug fix

--- main.py
+++ main.py
@@ -1,133 +1,198 @@
 import pygame
-import copy
 from settings import *
 from random import randint
 from pygame.locals import *
 
 pygame.font.init()
 
-# Create the screen
-screen = pygame.display.set_mode((WIDTH * SIZE_IMG, HEIGHT * SIZE_IMG))
 
-# Title, Icon and Settings
-pygame.display.set_caption("The snake game")
-icon = pygame.image.load("res/snake.png")
-pygame.display.set_icon(icon)
-score_font = pygame.font.SysFont("Britannic", 25, True)
-
-
-def move_block(block, vector, step: int):
-    if vector == "left" and block[0] >= step:
-        block[0] -= step
-    if vector == "right" and block[0] <= WIDTH - step:
-        block[0] += step
-    if vector == "up" and block[1] >= step:
-        block[1] -= step
-    if vector == "down" and block[1] <= HEIGHT - step:
-        block[1] += step
-    block[2] = vector
+def gen_x_y():
+    return randint(0, WIDTH - 1), randint(0, HEIGHT - 1)
 
 
 # Food Object
 class Food:
-    def __init__(self, screen):
+    def __init__(self, screen, x, y, color, size):
         self._screen = screen
+        self._color = color
+        self._size = size
         #self._img = pygame.image.load("res/apple.png")
-        self.x = randint(0, WIDTH - 1)
-        self.y = randint(0, HEIGHT - 1)
-        #self.img = pygame.transform.scale(self._img, (SIZE_IMG, SIZE_IMG))
+        self.x = x
+        self.y = y
+        #self.img = pygame.transform.scale(self._img, (self._size, self._size))
 
-    def regen(self):
-        self.x = randint(0, WIDTH - 1)
-        self.y = randint(0, HEIGHT - 1)
+    def regen(self, x, y):
+        self.x = x
+        self.y = y
 
     def draw(self):
-        #self._screen.blit(self.img, (self.x, self.y))
-        pygame.draw.rect(self._screen, RED, pygame.Rect(self.x * SIZE_IMG, self.y * SIZE_IMG, SIZE_IMG, SIZE_IMG))
-        #pygame.display.flip()
+        pygame.draw.rect(self._screen, self._color,
+                         pygame.Rect(self.x * self._size, self.y * self._size, self._size, self._size)
+                         )
+
+    def clear(self, back_color):
+        pygame.draw.rect(self._screen, back_color,
+                         pygame.Rect(self.x * self._size, self.y * self._size, self._size, self._size)
+                         )
+
+    def check(self, x, y):
+        # Check food position
+        return self.x == x and self.y == y
 
 
 # Snake Object
 class Snake:
-    def __init__(self, screen):
+    def __init__(self, screen, x, y, color, size_block, vector):
         self._screen = screen
-        self.body = []
+        self._vector = vector
+        self._size_block = size_block
+        self._color = color
         # add head
-        self.body.append([randint(0, WIDTH - 1),
-                          randint(0, HEIGHT - 1),
-                          VECTORS[randint(0, 3)]])
+        self.body = [{"x": x, "y": y}]
 
     @property
     def head(self):
         return self.body[0]
 
-    def move(self, vector):
+    @property
+    def vector(self):
+        return self._vector
+
+    @vector.setter
+    def vector(self, vector):
+        self._vector = vector
+
+    def _move_block(self, block, x, y):
+        prev_x, prev_y = block["x"], block["y"]
+        block["x"] = x
+        block["y"] = y
+        return prev_x, prev_y
+
+    def move(self, vector=None):
+        if vector:
+            self._vector = vector
         # Move a head
-        move_block(self.body[0], vector, 1)
+        delta_x, delta_y = direction_mapping[self._vector]
+        prev_x, prev_y = self.body[0]["x"], self.body[0]["y"]
+        self.body[0]["x"] += delta_x
+        self.body[0]["y"] += delta_y
         # Move a body
         for i in range(1, len(self.body)):
-            move_block(self.body[i], self.body[i-1][2], 1)
+            prev_x, prev_y = self._move_block(self.body[i], prev_x, prev_y)
+        return prev_x, prev_y
 
-    def grow(self):
-        self.body.append(self.body[-1].copy())
-        move_block(self.body[-1], self.body[-1][2], -1)
+    def grow(self, x, y):
+        last_block = self.body[-1].copy()
+        last_block["x"] = x
+        last_block["y"] = y
+        self.body.append(last_block)
 
     def draw(self):
         for block in self.body:
-            pygame.draw.rect(self._screen, GREEN, pygame.Rect(block[0] * SIZE_IMG,
-                                                              block[1] * SIZE_IMG,
-                                                              SIZE_IMG,
-                                                              SIZE_IMG))
+            pygame.draw.rect(self._screen, self._color, pygame.Rect(block["x"] * self._size_block,
+                                                                    block["y"] * self._size_block,
+                                                                    self._size_block,
+                                                                    self._size_block))
+
+    def clear(self, back_color):
+        for block in self.body:
+            pygame.draw.rect(self._screen, back_color, pygame.Rect(block["x"] * self._size_block,
+                                                                   block["y"] * self._size_block,
+                                                                   self._size_block,
+                                                                   self._size_block))
+
+    def check_pos(self, x, y):
+        for block in self.body:
+            if block["x"] == x and block["y"] == y:
+                return True
+        return False
 
 
-def print_score(score):
-    text = score_font.render("Score: " + str(score), True, ORANGE)
-    screen.blit(text, [0, 0])
+class Game:
+    def __init__(self, caption, icon_path):
+        pygame.init()
+        # Create the screen
+        self._screen = pygame.display.set_mode((WIDTH * SIZE_IMG, HEIGHT * SIZE_IMG))
+        # Title, Icon and Settings
+        pygame.display.set_caption(caption)
+        icon = pygame.image.load(icon_path)
+        pygame.display.set_icon(icon)
+        self._score_font = pygame.font.SysFont("Britannic", 25, True)
+        self._running = False
+        self._food, self._snake = None, None
+        self._score = 0
 
+    def check(self):
+        # Check head position
+        return self._snake.head["x"] < 0 or self._snake.head["x"] > WIDTH \
+                or self._snake.head["y"] < 0 or self._snake.head["y"] > HEIGHT
 
-def start_game():
+    def print_score(self):
+        text = self._score_font.render("Score: " + str(self._score), True, ORANGE)
+        self._screen.blit(text, [0, 0])
 
-    score = 0
-    food1 = Food(screen)
-    snake1 = Snake(screen)
+    def start_game(self):
+        self._score = 0
+        x, y = gen_x_y()
+        self._snake = Snake(self._screen,
+                            x,
+                            y,
+                            GREEN,
+                            SIZE_IMG,
+                            VECTORS[randint(0, 3)])
+        while self._snake.check_pos(x, y):
+            x, y = gen_x_y()
+        self._food = Food(self._screen, x, y, RED, SIZE_IMG)
+        self._running = True
 
-    def check_objects():
-        if False:
-            score += 1
+        while self._running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    match event.key:
+                        case pygame.K_ESCAPE:
+                            self._running = False
+                            break
+                        case pygame.K_LEFT:
+                            self._snake.vector = "left"
+                        case pygame.K_RIGHT:
+                            self._snake.vector = "right"
+                        case pygame.K_UP:
+                            self._snake.vector = "up"
+                        case pygame.K_DOWN:
+                            self._snake.vector = "down"
+                if event.type == QUIT:
+                    self._running = False
+                    break
+            if self._score >= 20:
+                self._running = False
+            if not self._running:
+                break
+            last_x, last_y = self._snake.move()
 
-    running = True
-    while running:
+            self._screen.fill(BLACK)
+            self.print_score()
+            text = self._score_font.render("Coord: " + str(self._snake.head), True, ORANGE)
+            self._screen.blit(text, [0, 30])
+            self._food.draw()
+            self._snake.draw()
+            pygame.display.update()
+            if self.check():
+                self._running = False
+                break
+            if self._food.check(**self._snake.head):
+                self._snake.grow(last_x, last_y)
+                x, y = gen_x_y()
+                while self._snake.check_pos(x, y):
+                    x, y = gen_x_y()
+                self._food.regen(x, y)
+                self._score += 1
+            pygame.time.wait(800 // (self._score + 1))
 
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                match event.key:
-                    case pygame.K_ESCAPE:
-                        running = False
-                    case pygame.K_LEFT:
-                        snake1.head[2] = "left"
-                    case pygame.K_RIGHT:
-                        snake1.head[2] = "right"
-                    case pygame.K_UP:
-                        snake1.head[2] = "up"
-                    case pygame.K_DOWN:
-                        snake1.head[2] = "down"
-            if event.type == QUIT:
-                running = False
-
-        if score >= 20:
-            running = False
-        snake1.move(snake1.head[2])
-        check_objects()
-        screen.fill(BLACK)
-        print_score(score)
-        food1.draw()
-        snake1.draw()
-        pygame.display.update()
-        pygame.time.wait(1000 // (score + 1))
-    pygame.quit()
+    def stop_game(self):
+        pygame.quit()
 
 
 if __name__ == "__main__":
-    pygame.init()
-    start_game()
-    pygame.quit()
+    game = Game("The Snake Game 1.0", ICON_PATH)
+    game.start_game()
+    game.stop_game()
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from random import randint
 from settings import *
-
+'''
 body_blocks = []
 body_blocks.append([randint(0, WIDTH - 1), randint(0, HEIGHT - 1), VECTORS[randint(0, 3)]])
 body_blocks.append([randint(0, WIDTH - 1), randint(0, HEIGHT - 1), VECTORS[randint(0, 3)]])
@@ -16,3 +16,20 @@ print(len(body_blocks))
 for i in range(1, len(body_blocks)):
     print(i)
     print(body_blocks[i])
+'''
+
+
+def _move_block(block, x, y):
+    prev_x, prev_y = block["x"], block["y"]
+    block["x"] = x
+    block["y"] = y
+    return prev_x, prev_y
+
+body = [{"x": 10, "y": 15}, {"x": 11, "y": 15}, {"x": 12, "y": 15}, {"x": 12, "y": 16}]
+delta_x, delta_y = direction_mapping["left"]
+prev_x, prev_y = body[0]["x"], body[0]["y"]
+body[0]["x"] += delta_x
+body[0]["y"] += delta_y
+# Move a body
+for i in range(1, len(body)):
+    prev_x, prev_y = _move_block(body[i], prev_x, prev_y)
